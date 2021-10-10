2c30ecfa39762c5fc78dea9c7a226000e9fc5c15
Tao Du <taodu@csail.mit.edu>
2021-10-09 22:39:11 -0400

    Disable no-bfgs in cantilever_3d.

--- python/example/cantilever_3d.py
+++ python/example/cantilever_3d.py
@@ -24,13 +24,11 @@ def test_cantilever_3d(verbose):
     # Setting thread number.
     thread_cts = [2, 4, 8, 12]
 
-    methods = ('newton_pcg', 'newton_cholesky', 'pd_eigen', 'pd_no_bfgs')
+    methods = ('newton_pcg', 'newton_cholesky', 'pd_eigen',)
     opts = ({ 'max_newton_iter': 5000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': 4 },
         { 'max_newton_iter': 5000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': 4 },
         { 'max_pd_iter': 5000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': 4,
-            'use_bfgs': 1, 'bfgs_history_size': 10 },
-        { 'max_pd_iter': 5000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': 4,
-            'use_bfgs': 0, 'bfgs_history_size': 10 })
+            'use_bfgs': 1, 'bfgs_history_size': 10 },)
 
     dofs = deformable.dofs()
     act_dofs = deformable.act_dofs()
